26c8fead16d3df1eea147506d1f33627ded2c1c7
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-09-29 14:25:39 +0500

    feat: complete array tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -600,8 +600,18 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  if (indexes.length === 0) {
+    return arr;
+  }
+
+  const [currentIndex, ...restIndexes] = indexes;
+
+  if (!Array.isArray(arr) || currentIndex < 0 || currentIndex >= arr.length) {
+    return undefined;
+  }
+
+  return getElementByIndexes(arr[currentIndex], restIndexes);
 }
 
 
@@ -623,8 +633,13 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const num = Math.floor(arr.length / 2);
+  const head = arr.slice(0, num);
+  const tail = arr.slice(-num);
+  const middle = (arr.length % 2 === 0 || arr.length === 1) ? [] : [arr[num]];
+  const result = [...tail, ...middle, ...head];
+  return result;
 }
 
 
