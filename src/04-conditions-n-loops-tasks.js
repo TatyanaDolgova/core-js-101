5b39557014a440ff1236ee9f74205f4413cc67b5
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-10-26 13:20:24 +0500

    feat: add some tasks

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -138,8 +138,15 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const top1 = rect1.top + rect1.height;
+  const left1 = rect1.left + rect1.width;
+
+  if (top1 < rect2.top || left1 < rect2.left) {
+    return false;
+  }
+
+  return true;
 }
 
 
@@ -169,8 +176,9 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const num = Math.sqrt((point.x - circle.center.x) ** 2 + (point.y - circle.center.y) ** 2);
+  return num < circle.radius;
 }
 
 
@@ -185,8 +193,21 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i += 1) {
+    let isUnique = true;
+    for (let j = 0; j < arr.length; j += 1) {
+      if (i !== j && str[i] === str[j]) {
+        isUnique = false;
+        break;
+      }
+    }
+    if (isUnique) {
+      return arr[i];
+    }
+  }
+  return null;
 }
 
 
